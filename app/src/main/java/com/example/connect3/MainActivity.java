eefab922b76c03954dd80400a95a6b1770c95aa5
Rams01010010 <ramsolaiappan@gmail.com>
2021-08-24 16:40:02 +0530

    Fixed a bug

--- app/src/main/java/com/example/connect3/MainActivity.java
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -15,14 +15,18 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     //0: yellow ; 1: red ; 2: Empty
+    //game variables
     int activePlayer = 0;
     int gameState[] = {2,2,2,2,2,2,2,2,2};
     int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     int flagForDrawMatch = 0;
     boolean gameActive = true;
+    
+    //Views
     TextView gameCurrentState;
     Button playAgainBtn;
-    //drop the counters in with animation
+    
+    //Counter touch function
     public void dropIn(View view)
     {
         ImageView counter = (ImageView) view;
@@ -66,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             Log.i("Flag : ",String.valueOf(flagForDrawMatch));
         }
         if(flagForDrawMatch == 9 && gameActive){
+            flagForDrawMatch = 0;
             gameCurrentState.setText("Draw Match!");
             playAgainBtn.setVisibility(View.VISIBLE);
         }
